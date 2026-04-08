013bd546d11c562a2cb70e2538a871f0f8d12f3b
DistinctDynamo <nguyensteven578@gmail.com>
2026-04-08 18:14:29 -0400

    testing again

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -7,6 +7,10 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Server
+  },
 ];
